ece2ef80c7d1764fc5762275d92d525974f2a52b
znn <15517007212@163.com>
2021-10-29 00:36:40 +0800

    second

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test1");
+        System.out.println("git test2");
     }
 }
---
+++ src/main/java/com/atguigu/Test2.java
@@ -0,0 +1,8 @@
+package com.atguigu;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("git test");
+
+    }
+}
